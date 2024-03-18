aac5181f4460e49cc37583587c259303536f1ef7
qwe515678 <mitflash56@gmail.com>
2024-03-18 17:32:18 +0300

    added icons

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,10 +2,11 @@ import createMiddleware from "next-intl/middleware";
 
 export default createMiddleware({
     locales: ['ru', 'en', 'de'],
-    defaultLocale: 'ru'
+    defaultLocale: 'ru',
+    localeDetection: true
 })
 
 
 export const config = {
     matcher: ['/', '/(ru|en|de)/:path*']
-}
+} 
